676361a8830b2639e89ccdba8a2f27c8d67d14f5
Robert Luby <rluby@redhat.com>
2026-06-19 11:22:25 +0200

    feat: initialize Knative function project

---
+++ function/func.py
@@ -0,0 +1,6 @@
+from parliament import Context
+
+
+def main(context: Context):
+    body = context.request.json if context.request.content_type == "application/json" else {}
+    return {"message": "Hello from test-func-python!", "input": body}, 200
