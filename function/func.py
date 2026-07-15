c2bf753397fe13d24bbdde07ca53d2c2d5649d6d
Robert Luby <rluby@redhat.com>
2026-07-15 14:08:48 +0200

    Update function files

--- function/func.py
+++ function/func.py
@@ -4,4 +4,4 @@ from parliament import Context
 def main(context: Context):
     body = context.request.json if context.request.content_type == "application/json" else {}
     return {"message": "Hello from test-func-python!", "input": body}, 200
-// save test// save test// save test// save test
+// save test// save test// save test// save test// save test
